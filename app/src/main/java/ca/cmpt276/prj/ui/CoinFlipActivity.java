e4767e9ac87c4915206f9197c8c2c443f51d4ccd
yuethany <ethan_yu@sfu.ca>
2020-10-31 20:49:30 -0700

    Added heads and tails coin images, and coin flipping animation

--- app/src/main/java/ca/cmpt276/prj/ui/CoinFlipActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/CoinFlipActivity.java
@@ -1,11 +1,19 @@
 package ca.cmpt276.prj.ui;
 
+/*
+    Coin Flip activity, child calls heads or tails
+    and the coin flips, displaying result of flip
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -14,13 +22,14 @@ import ca.cmpt276.prj.R;
 
 public class CoinFlipActivity extends AppCompatActivity {
     private String child = "EXAMPLE_CHILD";
+    private ImageView coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
+        coin = findViewById(R.id.coinImage);
         setupHeadsButton();
         setupTailsButton();
     }
@@ -37,6 +46,7 @@ public class CoinFlipActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectHeads();
+                flipCoinAnimation();
             }
         });
     }
@@ -47,6 +57,7 @@ public class CoinFlipActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectTails();
+                flipCoinAnimation();
             }
         });
     }
@@ -59,6 +70,39 @@ public class CoinFlipActivity extends AppCompatActivity {
     private void selectTails(){
         TextView selection = findViewById(R.id.selectionDetails);
         selection.setText(getString(R.string.selectionTails, child));
+    }
+
+    private void flipCoinAnimation(){
+        ObjectAnimator flipAnimation = ObjectAnimator.ofFloat(coin, "rotationX", 0f, 1800f);
+        long animationDuration = 3000;
+        flipAnimation.setDuration(animationDuration);
+        flipAnimation.addListener(new Animator.AnimatorListener(){
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                setFlipResult(0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        flipAnimation.start();
+    }
+
+    private void setFlipResult(int result){
+        if(result == 0){
+            coin.setImageResource(R.drawable.heads_coin);
+        } else {
+            coin.setImageResource(R.drawable.tails_coin);
+        }
     }
 
 
