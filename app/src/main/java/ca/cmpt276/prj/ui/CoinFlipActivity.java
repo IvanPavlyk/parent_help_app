bb9a0ec640cea81f617ee507a6a2a8ea1a076d49
yuethany <ethan_yu@sfu.ca>
2020-10-31 18:46:55 -0700

    created Coin Flip Activity and mocked up basic layout

---
+++ app/src/main/java/ca/cmpt276/prj/ui/CoinFlipActivity.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.prj.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.prj.R;
+
+public class CoinFlipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip);
+    }
+}
