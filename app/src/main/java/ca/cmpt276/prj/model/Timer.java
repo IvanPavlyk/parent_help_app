edc6610fcfd2367cfcda99a6242ceed53771801f
duleid <duleid@sfu.ca>
2020-11-01 20:47:13 -0800

    Timeout Timer initial

---
+++ app/src/main/java/ca/cmpt276/prj/model/Timer.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.prj.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.prj.R;
+
+public class Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
