430b97b988532411a0e2e27861fb959a1d75219d
duleid <duleid@sfu.ca>
2020-11-02 19:40:59 -0800

    Timeout Timer UI adjust

--- app/src/main/java/ca/cmpt276/prj/ui/MainActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/MainActivity.java
@@ -1,10 +1,14 @@
 package ca.cmpt276.prj.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import ca.cmpt276.prj.R;
+import ca.cmpt276.prj.model.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTimer();
+    }
+
+    private void setTimer() {
+        Button btn = findViewById(R.id.test);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Timer.class);
+                startActivity(i);
+            }
+        });
     }
 }
