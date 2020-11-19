d5356e0da55f769b964611042c0f628f784ea91d
duleid <duleid@sfu.ca>
2020-11-19 01:49:59 -0800

    read data from input

--- app/src/main/java/ca/cmpt276/prj/ui/WhoseTurnActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/WhoseTurnActivity.java
@@ -1,8 +1,13 @@
 package ca.cmpt276.prj.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import ca.cmpt276.prj.R;
 
@@ -12,5 +17,32 @@ public class WhoseTurnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whose_turn);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setDisplayHomeAsUpEnabled(true);
+            bar.setTitle("Timeout Timer");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.save_button:
+                EditText make=(EditText)findViewById(R.id.addName);
+                String n = make.getText().toString();
+
+                EditText aperture=(EditText)findViewById(R.id.addTask);
+                String t = aperture.getText().toString();
+
+                EditText focal=(EditText)findViewById(R.id.addDescription);
+                String d=focal.getText().toString();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
