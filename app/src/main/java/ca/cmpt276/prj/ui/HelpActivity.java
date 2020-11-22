d5c40bffe488dd6171406dc599c23edecdf641a7
Builderbot2000 <kevintang1118@outlook.com>
2020-11-21 19:33:52 -0800

    Implemented Help Page
    Completely implemented the help page which can be accessed through the help button. It displays a brief description of the app, credits, and copyright information.

---
+++ app/src/main/java/ca/cmpt276/prj/ui/HelpActivity.java
@@ -0,0 +1,35 @@
+package ca.cmpt276.prj.ui;
+import ca.cmpt276.prj.R;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
+
+public class HelpActivity extends AppCompatActivity {
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, HelpActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_main);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
